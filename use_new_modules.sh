deff7e5dd33b3367dcce27046552d7b87d11b0aa
AaronTHolt <holtat@colorado.edu>
2016-01-22 12:10:19 -0700

    Add script to enable modules and main jupyterhub config file

---
+++ use_new_modules.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source /curc/tools/x86_64/rh6/software/lmod/lmod/init/profile
+export LMOD_PACKAGE_PATH=/curc/tools/x86_64/rh6/modules/.site
+export MODULEPATH=/curc/tools/x86_64/rh6/modules/pydep/python/3.4.3/intel/15.0.2:/curc/tools/x86_64/rh6/modules/mpis/intel/15.0.2:/curc/tools/x86_64/rh6/modules/cdep/intel/15.0.2:/curc/tools/x86_64/rh6/modules/compilers:/curc/tools/x86_64/rh6/modules/idep:/curc/tools/x86_64/rh6/software/lmod/lmod/modulefiles/Core
