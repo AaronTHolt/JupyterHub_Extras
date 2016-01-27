8796fe36398f953cfaef6b7078f23b5d01d0f4d6
AaronTHolt <holtat@colorado.edu>
2016-01-27 09:37:23 -0700

    Add ipython config creation script for Jupyter2

---
+++ createIpythonDefaultConfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ipythonDir="/home/$USER/.ipython.rc"
+configDir="/curc/tools/x86_64/rh6/software/python_packages/3.4.3/intel/15.0.2/jupyterhub/0.2.0/Ipython"
+
+#check if dir exists
+if [ -d $ipythonDir ]; then
+    #check for symlink/file
+    if [ -L $ipythonDir ]; then
+        echo "Error, symlink at ipythonDir"
+    elif [ -f $ipythonDir ]; then
+        echo "Error, file at ipythonDir"
+    else
+        echo "Directory already exists"
+    fi
+else
+    #directory doesn't exist, create it, copy config
+    if mkdir $ipythonDir; then
+        echo "Directory successfully created"
+    else 
+        echo "Directory creation failed" 
+    fi
+
+    #Copy ipython config files
+    if cp -rn $configDir/* $ipythonDir/; then
+        echo "Files successfully copied"
+    else 
+        echo "File copy failed" 
+    fi
+fi
